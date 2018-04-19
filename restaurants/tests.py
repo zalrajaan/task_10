ca9e63965047645fe31eed4f1210439243c48d05
DarthHamza <hamzamakia@gmail.com>
2018-04-19 14:50:45 +0300

    solution with test files

--- restaurants/tests.py
+++ restaurants/tests.py
@@ -1,73 +1,146 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
 from django.contrib.auth.models import User
-from restaurants.models import Restaurant
-from restaurants.forms import RestaurantForm, SignupForm, SigninForm
+from restaurants.models import Restaurant, Item
+from restaurants.forms import RestaurantForm, ItemForm, SignupForm, SigninForm
+from restaurants.views import restaurant_create
 
-class RestaurantModelTestCase(TestCase):
-    def test_create(self):
-        Restaurant.objects.create(
-            name="Hamza's Pizza",
-            description="Pizza that tastes really good.",
-            opening_time="00:01:00",
-            closing_time="23:59:00",
-            logo="http://icons.veryicon.com/png/Movie%20%26%20TV/Free%20Star%20Wars/Darth%20Vader.png",
-            )
-
-class RestaurantViewTestCase(TestCase):
+class ModelTestCase(TestCase):
     def setUp(self):
-        self.data = {
-            "name": "Hamza's Pizza",
-            "description": "Pizza that tastes really good.",
-            "opening_time": "00:01:00",
-            "closing_time":"23:59:00",
-            "logo":"http://icons.veryicon.com/png/Movie%20%26%20TV/Free%20Star%20Wars/Darth%20Vader.png"
-            }
-        self.restaurant_1 = Restaurant.objects.create(
-            name="Restaurant 1",
-            description="This is Restaurant 1",
-            opening_time="00:01:00",
-            closing_time="23:59:00",
-            logo="http://icons.veryicon.com/png/Movie%20%26%20TV/Free%20Star%20Wars/Darth%20Vader.png"
-            )
-        self.restaurant_2 = Restaurant.objects.create(
-            name="Restaurant 2", 
-            description="This is Restaurant 2",
-            opening_time="00:01:00",
-            closing_time="23:59:00",
-            logo="http://icons.veryicon.com/png/Movie%20%26%20TV/Free%20Star%20Wars/Darth%20Vader.png"
-            )
-        self.restaurant_3 = Restaurant.objects.create(
-            name="Restaurant 3",
-            description="This is Restaurant 3",
-            opening_time="00:01:00",
-            closing_time="23:59:00",
-            logo="http://icons.veryicon.com/png/Movie%20%26%20TV/Free%20Star%20Wars/Darth%20Vader.png"
-            )
         self.user = User.objects.create(
             username="bob",
             password='adminadmin',
             )
         self.user.set_password(self.user.password)
         self.user.save()
-        self.user_1 = {
+
+    def test_create(self):
+        restaurant = Restaurant.objects.create(
+            owner= self.user,
+            name="Hamza's Pizza",
+            description="Pizza that tastes really good.",
+            opening_time="00:01:00",
+            closing_time="23:59:00",
+            logo="http://icons.veryicon.com/png/Movie%20%26%20TV/Free%20Star%20Wars/Darth%20Vader.png",
+            )
+        item = Item.objects.create(
+            name="Hamza's Special Pizza",
+            description="The restaurant's special.",
+            price=1.750,
+            restaurant=restaurant,
+            )
+    
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(
+            username="bob",
+            password='adminadmin',
+            )
+        self.user.set_password(self.user.password)
+        self.user.save()
+        self.user2 = User.objects.create(
+            username="bob2",
+            password='adminadmin',
+            )
+        self.user2.set_password(self.user2.password)
+        self.user2.save()
+
+        self.restaurant_data = {
+            "name": "Hamza's Pizza",
+            "description": "Pizza that tastes really good.",
+            "opening_time": "00:01:00",
+            "closing_time":"23:59:00",
+            "logo":"http://icons.veryicon.com/png/Movie%20%26%20TV/Free%20Star%20Wars/Darth%20Vader.png"
+            }
+        self.item_data = {
+            "name": "Original Pizza",
+            "description": "Best original pizza ever.",
+            "price": 1.750,
+        }
+
+        self.restaurant_1 = Restaurant.objects.create(
+            owner=self.user,
+            name="Restaurant 1",
+            description="This is Restaurant 1",
+            opening_time="00:01:00",
+            closing_time="23:59:00",
+            logo="http://icons.veryicon.com/png/Movie%20%26%20TV/Free%20Star%20Wars/Darth%20Vader.png"
+            )
+        self.item_1_1 = Item.objects.create(
+            name="Pizza 1",
+            description="This is Pizza 1",
+            price=1.750,
+            restaurant=self.restaurant_1,
+            )
+        self.item_1_2 = Item.objects.create(
+            name="Pizza 2",
+            description="This is Pizza 2",
+            price=1.750,
+            restaurant=self.restaurant_1,
+            )
+
+        self.restaurant_2 = Restaurant.objects.create(
+            owner=self.user2,
+            name="Restaurant 2", 
+            description="This is Restaurant 2",
+            opening_time="00:01:00",
+            closing_time="23:59:00",
+            logo="http://icons.veryicon.com/png/Movie%20%26%20TV/Free%20Star%20Wars/Darth%20Vader.png"
+            )
+        self.item_2_1 = Item.objects.create(
+            name="Pizza 1",
+            description="This is Pizza 1",
+            price=1.750,
+            restaurant=self.restaurant_2,
+            )
+        self.item_2_2 = Item.objects.create(
+            name="Pizza 2",
+            description="This is Pizza 2",
+            price=1.750,
+            restaurant=self.restaurant_2,
+            )
+
+        self.restaurant_3 = Restaurant.objects.create(
+            owner=self.user,
+            name="Restaurant 3",
+            description="This is Restaurant 3",
+            opening_time="00:01:00",
+            closing_time="23:59:00",
+            logo="http://icons.veryicon.com/png/Movie%20%26%20TV/Free%20Star%20Wars/Darth%20Vader.png"
+            )
+        self.item_3_1 = Item.objects.create(
+            name="Pizza 1",
+            description="This is Pizza 1",
+            price=1.750,
+            restaurant=self.restaurant_3,
+            )
+        self.item_3_2 = Item.objects.create(
+            name="Pizza 2",
+            description="This is Pizza 2",
+            price=1.750,
+            restaurant=self.restaurant_3,
+            )
+        
+        self.user_data_1 = {
             "username": "bob",
             "password": "adminadmin"
             }
-        self.user_2 = {
+        self.user_data_2 = {
             "username": "billy",
             "password": "adminadmin",
             }
-        self.user_3 = {
+        self.user_data_3 = {
             "username": "bob",
             "password": "",
             }
-        self.user_4 = {
+        self.user_data_4 = {
             "username": "",
             "password": "somepassword",
             }
 
-    def test_list_view(self):
+    def test_restaurant_list_view(self):
         list_url = reverse("restaurant-list")
         response = self.client.get(list_url)
         for restaurant in Restaurant.objects.all():
@@ -78,32 +151,52 @@ class RestaurantViewTestCase(TestCase):
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(response.status_code, 200)
 
-    def test_detail_view(self):
+    def test_restaurant_detail_view(self):
         detail_url = reverse("restaurant-detail", kwargs={"restaurant_id":self.restaurant_1.id})
         response = self.client.get(detail_url)
-        self.assertEqual(self.restaurant_1, response.context['restaurant'])
         self.assertContains(response, self.restaurant_1.name)
         self.assertContains(response, self.restaurant_1.description)
         self.assertContains(response, self.restaurant_1.logo.url)
+        for item in Item.objects.filter(restaurant=self.restaurant_1):
+            self.assertContains(response, item.name)
+            self.assertContains(response, item.description)
+            self.assertContains(response, item.price)
         self.assertTemplateUsed(response, 'detail.html')
         self.assertEqual(response.status_code, 200)
 
-    def test_create_view(self):
+    def test_restaurant_create_view(self):
         create_url = reverse("restaurant-create")
-        response = self.client.get(create_url)
+        request = self.factory.get(create_url)
+        request.user = self.user
+        response = restaurant_create(request)
         self.assertEqual(response.status_code, 200)
-        response2 = self.client.post(create_url, self.data)
+
+        request2 = self.factory.post(create_url, self.restaurant_data)
+        request2.user = self.user
+        response2 = restaurant_create(request2)
         self.assertEqual(response2.status_code, 302)
 
-    def test_update_view(self):
+    def test_item_create_view(self):
+        create_url = reverse("item-create", kwargs={"restaurant_id":self.restaurant_1.id})
+        response = self.client.get(create_url)
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(create_url, data=self.item_data)
+        self.assertEqual(response.status_code, 302)
+
+        detail_url = reverse("restaurant-detail", kwargs={"restaurant_id":self.restaurant_1.id})
+        response = self.client.get(detail_url)
+        self.assertTrue(Item.objects.filter(restaurant=self.restaurant_1, name="Original Pizza").exists())
+
+    def test_restaurant_update_view(self):
         update_url = reverse("restaurant-update", kwargs={"restaurant_id":self.restaurant_1.id})
         response = self.client.get(update_url)
         self.assertEqual(response.status_code, 200)
 
-        response = self.client.post(update_url, data=self.data)
+        response = self.client.post(update_url, data=self.restaurant_data)
         self.assertEqual(response.status_code, 302)
 
-    def test_delete_view(self):
+    def test_restaurant_delete_view(self):
         delete_url = reverse("restaurant-delete", kwargs={"restaurant_id":self.restaurant_1.id})
         response = self.client.get(delete_url)
         self.assertEqual(response.status_code, 302)
@@ -114,16 +207,16 @@ class RestaurantViewTestCase(TestCase):
         response = self.client.get(signup_url)
         self.assertEqual(response.status_code, 200)
 
-        response2 = self.client.post(signup_url, self.user_1)
+        response2 = self.client.post(signup_url, self.user_data_1)
         self.assertEqual(response2.status_code, 200)
 
-        response3 = self.client.post(signup_url, self.user_2)
+        response3 = self.client.post(signup_url, self.user_data_2)
         self.assertEqual(response3.status_code, 302)
 
-        response4 = self.client.post(signup_url, self.user_3)
+        response4 = self.client.post(signup_url, self.user_data_3)
         self.assertEqual(response4.status_code, 200)
 
-        response5 = self.client.post(signup_url, self.user_4)
+        response5 = self.client.post(signup_url, self.user_data_4)
         self.assertEqual(response5.status_code, 200)
 
     def test_signin_view(self):
@@ -132,16 +225,16 @@ class RestaurantViewTestCase(TestCase):
         response = self.client.get(signin_url)
         self.assertEqual(response.status_code, 200)
 
-        response2 = self.client.post(signin_url, self.user_1)
+        response2 = self.client.post(signin_url, self.user_data_1)
         self.assertEqual(response2.status_code, 302)
 
-        response2 = self.client.post(signin_url, self.user_2)
+        response2 = self.client.post(signin_url, self.user_data_2)
         self.assertEqual(response2.status_code, 200)
 
-        response2 = self.client.post(signin_url, self.user_3)
+        response2 = self.client.post(signin_url, self.user_data_3)
         self.assertEqual(response2.status_code, 200)
 
-        response2 = self.client.post(signin_url, self.user_4)
+        response2 = self.client.post(signin_url, self.user_data_4)
         self.assertEqual(response2.status_code, 200)
 
     def test_signout_view(self):
@@ -179,51 +272,90 @@ class RestaurantFormTestCase(TestCase):
         form = RestaurantForm(data=data)
         self.assertFalse(form.is_valid())
 
+class ItemFormTestCase(TestCase):
+    def test_valid_form(self):
+        name = "A Pizza Shop"
+        description = "Best pizza shop in the neighbourhood."
+        price = 1.750
+        data = {
+            'name':name,
+            'description': description,
+            'price': price,
+        }
+        form = ItemForm(data=data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data.get('name'), name)
+        self.assertEqual(form.cleaned_data.get('description'), description)
+        self.assertEqual(form.cleaned_data.get('price'), price)
+
+    def test_invalid_form(self):
+        name = "A Pizza Shop"
+        description = "Best pizza shop in the neighbourhood."
+        price = 1.750
+        data = {
+            'name':name,
+            'description': description,
+        }
+        data2 = {
+            'name':name,
+            'price': price,
+        }
+        data3 = {
+            'price':price,
+            'description': description,
+        }
+        form = ItemForm(data=data)
+        form2 = ItemForm(data=data2)
+        form3 = ItemForm(data=data3)
+        self.assertFalse(form.is_valid())
+        self.assertFalse(form2.is_valid())
+        self.assertFalse(form3.is_valid())
+
 class AuthFormTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create(
             username="bob",
             password='adminadmin',
         )
-        self.user_1 = {
+        self.user_data_1 = {
             "username": "bob",
             "password": "adminadmin",
         }
-        self.user_2 = {
+        self.user_data_2 = {
             "username": "billy",
             "password": "adminadmin",
         }
-        self.user_3 = {
+        self.user_data_3 = {
             "username": "bob",
             "password": "",
         }
-        self.user_4 = {
+        self.user_data_4 = {
             "username": "",
             "password": "somepassword",
         }
 
     def test_valid_signin_form(self):
-        form = SigninForm(data=self.user_1)
+        form = SigninForm(data=self.user_data_1)
         self.assertTrue(form.is_valid())
-        self.assertEqual(form.cleaned_data.get('username'), self.user_1['username'])
-        self.assertEqual(form.cleaned_data.get('password'), self.user_1['password'])
+        self.assertEqual(form.cleaned_data.get('username'), self.user_data_1['username'])
+        self.assertEqual(form.cleaned_data.get('password'), self.user_data_1['password'])
 
     def test_invalid_signin_form(self):
-        form = SigninForm(data=self.user_3)
+        form = SigninForm(data=self.user_data_3)
         self.assertFalse(form.is_valid())
-        form = SigninForm(data=self.user_4)
+        form = SigninForm(data=self.user_data_4)
         self.assertFalse(form.is_valid())
 
     def test_valid_signup_form(self):
-        form = SignupForm(data=self.user_2)
+        form = SignupForm(data=self.user_data_2)
         self.assertTrue(form.is_valid())
-        self.assertEqual(form.cleaned_data['username'], self.user_2['username'])
-        self.assertEqual(form.cleaned_data.get('password'), self.user_2['password'])
+        self.assertEqual(form.cleaned_data['username'], self.user_data_2['username'])
+        self.assertEqual(form.cleaned_data.get('password'), self.user_data_2['password'])
 
     def test_invalid_signup_form(self):
-        form = SignupForm(data=self.user_1)
+        form = SignupForm(data=self.user_data_1)
         self.assertFalse(form.is_valid())
-        form = SignupForm(data=self.user_3)
+        form = SignupForm(data=self.user_data_3)
         self.assertFalse(form.is_valid())
-        form = SignupForm(data=self.user_4)
+        form = SignupForm(data=self.user_data_4)
         self.assertFalse(form.is_valid())
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Restaurant
-from .forms import RestaurantForm, SignupForm, SigninForm
+from .models import Restaurant, Item
+from .forms import RestaurantForm, ItemForm, SignupForm, SigninForm
 from django.contrib.auth import login, authenticate, logout
 
 def signup(request):
@@ -50,8 +50,11 @@ def restaurant_list(request):
 
 
 def restaurant_detail(request, restaurant_id):
+    restaurant = Restaurant.objects.get(id=restaurant_id)
+    items = Item.objects.filter(restaurant=restaurant)
     context = {
-        "restaurant": Restaurant.objects.get(id=restaurant_id)
+        "restaurant": restaurant,
+        "items": items,
     }
     return render(request, 'detail.html', context)
 
@@ -60,12 +63,30 @@ def restaurant_create(request):
     if request.method == "POST":
         form = RestaurantForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            restaurant = form.save(commit=False)
+            restaurant.owner = request.user
+            restaurant.save()
             return redirect('restaurant-list')
     context = {
         "form":form,
     }
     return render(request, 'create.html', context)
+
+def item_create(request, restaurant_id):
+    form = ItemForm()
+    restaurant = Restaurant.objects.get(id=restaurant_id)
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.restaurant = restaurant
+            item.save()
+            return redirect('restaurant-detail', restaurant_id)
+    context = {
+        "form":form,
+        "restaurant": restaurant,
+    }
+    return render(request, 'item_create.html', context)
 
 def restaurant_update(request, restaurant_id):
     restaurant_obj = Restaurant.objects.get(id=restaurant_id)
